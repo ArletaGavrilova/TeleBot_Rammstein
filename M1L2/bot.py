098fb56eaa9bdd48ece98d9b47fa0b9629ca981d
ArletaGavrilova <109897868+ArletaGavrilova@users.noreply.github.com>
2024-12-12 18:34:02 +0200

    Add files via upload

---
+++ M1L2/bot.py
@@ -0,0 +1,32 @@
+import telebot
+import config
+from random import choice
+
+bot = telebot.TeleBot(config.token)
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['coin'])
+def coin_handler(message):
+    coin = choice(["ОРЕЛ", "РЕШКА"])
+    bot.reply_to(message, coin)
+
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.reply_to(message, """
+You want more info? Okay, I got it... 
+Here is you info: you can type help or start get to know more about this bot.""")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
---
+++ M1L2/config.py
@@ -0,0 +1,1 @@
+token = '7537587684:AAHfBduok1xEr0RkiT_g63XPKPod6QJXYxc'
